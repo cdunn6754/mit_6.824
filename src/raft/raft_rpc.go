2909a5c636c3f8c7edbbfe6b8d2abeda7b6a4776
Clint <cdunn6754@gmail.com>
2022-10-25 08:08:10 -0700

    Get rid of an early exit to prepare for changes in handling the commitIdx

--- src/raft/raft_rpc.go
+++ src/raft/raft_rpc.go
@@ -77,20 +77,18 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 		}
 	}
 
-	if args.Entries == nil || len(args.Entries) == 0 {
-		// Nothing to do, this is probably a heartbeat, there was no new data to send
-		return
-	}
-	// For the time being only one log entry at a time is supported, TODO optimization here
-	if len(args.Entries) > 1 {
-		log.Panic("Sending more than one command at a time is not supported")
-	}
-	entry := args.Entries[0]
+	if args.Entries != nil || len(args.Entries) > 0 {
+		// For the time being only one log entry at a time is supported, TODO optimization here
+		if len(args.Entries) > 1 {
+			log.Panic("Sending more than one command at a time is not supported")
+		}
+		entry := args.Entries[0]
 
-	// TODO: it might be better to check if this entry already exists here, but this accomplishes getting
-	// rid of unwanted logs and adding the new one simply
-	rf.updateLog(append(rf.log[:args.PrevLogIndex], entry))
-	log.Printf("Raft %d adding entry %v to log at index %d", rf.me, entry.Command, len(rf.log))
+		// TODO: it might be better to check if this entry already exists here, but this accomplishes getting
+		// rid of unwanted logs and adding the new one simply
+		rf.updateLog(append(rf.log[:args.PrevLogIndex], entry))
+		log.Printf("Raft %d adding entry %v to log at index %d", rf.me, entry.Command, len(rf.log))
+	}
 }
 
 func (rf *Raft) sendAppendEntries(peerIdx int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
